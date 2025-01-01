7b3bc170c875a7d7e447b39cc794bb8112ced3e6
bumblebee <benjamin-_-1111@outlook.com>
2025-01-02 00:36:11 +0800

    uncomment implemented

--- plugin/Comment.lua
+++ plugin/Comment.lua
@@ -11,6 +11,7 @@
 -- * zero or more
 -- + one or more
 -- - zero or one
+
 local function extension()
     local fileName=vim.api.nvim_buf_get_name(0)
     return fileName:match("^.+(%..+)$")
@@ -32,12 +33,17 @@ local function comment()
     return
   end
   local cmt=cmtStr(ext)
+  local l=cmt:len()
 
   -- get_cursor: top right corner is (1, 0)
   local r=vim.api.nvim_win_get_cursor(0)[1]
   local line=vim.api.nvim_buf_get_lines(0, r-1, r, false)[1]
   local i=#(line:match("^%s*"))
-  vim.api.nvim_buf_set_text(0, r-1, i, r-1, i, {cmt})
+  if line:sub(i, i+l)==cmt then
+    vim.api.nvim_buf_set_text(0, r-1, i, r-1, i+l, {""})
+  else
+    vim.api.nvim_buf_set_text(0, r-1, i, r-1, i, {cmt})
+  end
 end
 
 vim.keymap.set('n', '<M-,>', comment, {noremap = true})
