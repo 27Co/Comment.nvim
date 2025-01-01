6a61af815b80270ec151f100fd84a3f9b8aa8eb4
bumblebee <benjamin-_-1111@outlook.com>
2025-01-01 22:29:51 +0800

    Copied from init.lua into plugin/Comment.nvim

---
+++ plugin/Comment.lua
@@ -0,0 +1,44 @@
+
+-- string pattern matching
+-- characters:
+-- . any character
+-- %s whitespace
+-- %d digit
+-- %a letter
+-- anchors:
+-- ^ beginning of line
+-- $ end of line
+-- modifiers:
+-- * zero or more
+-- + one or more
+-- - zero or one
+local function extension()
+    local fileName=vim.api.nvim_buf_get_name(0)
+    return fileName:match("^.+(%..+)$")
+end
+
+local function cmtStr(ext)
+    if string.match(ext, "%.c.*") then
+        return "// "
+    elseif string.match(ext, "%.lua") then
+        return "-- "
+    else
+        return ""
+    end
+end
+
+local function comment()
+  local ext=extension()
+  if ext==nil then
+    return
+  end
+  local cmt=cmtStr(ext)
+
+  -- get_cursor: top right corner is (1, 0)
+  local r=vim.api.nvim_win_get_cursor(0)[1]
+  local line=vim.api.nvim_buf_get_lines(0, r-1, r, false)[1]
+  local i=#(line:match("^%s*"))
+  vim.api.nvim_buf_set_text(0, r-1, i, r-1, i, {cmt})
+end
+
+vim.keymap.set('n', '<leader>/', comment, {noremap = true})
